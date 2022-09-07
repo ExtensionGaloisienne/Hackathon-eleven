5dba3dc67fbddaab7e7899f376d6d6581db340b1
Leonard Grellier <leonard.grellier@gmail.com>
2022-09-07 14:02:01 +0200

    sais plus ce que j'ai fait

--- heatmap/heatmap.py
+++ heatmap/heatmap.py
@@ -9,11 +9,14 @@ def choose_random_file(directory):
     return random.choice(os.listdir(directory))
 
 # Choix d'un fichier json aléatoire
-def choose_random_json(directory):
+def get_random_json_path(directory):
     return os.path.join(directory, choose_random_file(directory))
 
 def heatmap():
+    random.seed()
     directory = "C:\\Users\\Léonard\\Downloads\\data\\Detection_Test_Set\\Detection_Test_Set_Json"
-    json_file = choose_random_json(directory)
+    json_file = get_random_json_path(directory)
     print(json_file)
     return 0
+
+heatmap()
