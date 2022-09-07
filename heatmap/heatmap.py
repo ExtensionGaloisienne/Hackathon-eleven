0823c2050096eb832db7bcbf60ef9b8b01844a43
Leonard Grellier <leonard.grellier@gmail.com>
2022-09-07 15:24:57 +0200

    Version correcte pour lire les fichiers json

--- heatmap/heatmap.py
+++ heatmap/heatmap.py
@@ -5,22 +5,32 @@ import random
 import json
 
 
-#Choix d'un fichier aléatoire
+# Choix d'un fichier aléatoire
 def choose_random_file(directory):
     return random.choice(os.listdir(directory))
+
 
 # Choix d'un fichier json aléatoire
 def get_random_json_path(directory):
     return os.path.join(directory, choose_random_file(directory))
 
+
+def get_all_json_paths(directory):
+    return [os.path.join(directory, json_file) for json_file in os.listdir(directory)]
+
+
 def heatmap():
     random.seed()
     directory = "C:\\Users\\Léonard\\Downloads\\data\\Detection_Test_Set\\Detection_Test_Set_Json"
-    json_file = get_random_json_path(directory)
-    print(json_file)
-    with open(json_file) as f:
-        json_data = json.load(f)
-    print(json_data)
+    json_files = get_all_json_paths(directory)
+    json_data_list = []
+    for json_file in json_files:
+        with open(json_file) as f:
+            try:
+                json_data = json.load(f)
+                json_data_list.append(json_data)
+            except json.JSONDecodeError:
+                print("Couldn't decode file {}".format(json_file))
     return 0
 
 heatmap()
