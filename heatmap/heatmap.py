dc01316fa64d120581bb598178edc0ffec3dc177
Leonard Grellier <leonard.grellier@gmail.com>
2022-09-07 18:30:42 +0200

    répartition des fichiers suivant les chantiers, mais encore plus

--- heatmap/heatmap.py
+++ heatmap/heatmap.py
@@ -10,6 +10,11 @@ def choose_random_file(directory):
     return random.choice(os.listdir(directory))
 
 
+def show_json_data(json_list):
+    for x in json_list:
+        print(x)
+
+
 # Choix d'un fichier json aléatoire
 def get_random_json_path(directory):
     return os.path.join(directory, choose_random_file(directory))
@@ -19,15 +24,17 @@ def get_all_json_paths(directory):
     return [os.path.join(directory, json_file) for json_file in os.listdir(directory)]
 
 
+def filter_by_chantier(json_files, name):
+    return [x for x in json_files if name in x]
 def heatmap():
     random.seed()
     directory = r'C:\Users\Léonard\Downloads\data\Detection_Train_Set\Detection_Train_Set_Json'
     json_files = get_all_json_paths(directory)
-    devisubox_files = [x for x in json_files if "Devisubox" in x]
-    marseille_files = [x for x in json_files if "Marseille" in x]
-    biosav_files = [x for x in json_files if "BioSAV" in x]
-    nouveaucampus_files = [x for x in json_files if "Nouveau" in x]
-    roissy_files = [x for x in json_files if "Roissy" in x]
+    devisubox_files = filter_by_chantier(json_files, "Devisubox")
+    marseille_files = filter_by_chantier(json_files, "Marseille")
+    biosav_files = filter_by_chantier(json_files, "BioSAV")
+    nouveaucampus_files = filter_by_chantier(json_files, "Nouveau")
+    roissy_files = filter_by_chantier(json_files, "Roissy")
     print("Total: {}, BioSAV: {}, Devisubox: {}, Marseille: {}\
 , Nouveau Campus: {}, Roissy: {}".format(len(json_files),
                                              len(biosav_files),
@@ -35,14 +42,16 @@ def heatmap():
                                              len(marseille_files),
                                              len(nouveaucampus_files),
                                              len(roissy_files)))
-    json_data_list = []
-    for json_file in json_files:
-        with open(json_file) as f:
-            try:
-                json_data = json.load(f)
-                json_data_list.append(json_data)
-            except json.JSONDecodeError:
-                print("Couldn't decode file {}".format(json_file))
+    chantiers = [devisubox_files, marseille_files, biosav_files, nouveaucampus_files, roissy_files]
+    data_by_chantier = [[] for x in chantiers]
+    for i in range(len(chantiers)):
+        for json_file in chantiers[i]:
+            with open(json_file) as f:
+                try:
+                    json_data = json.load(f)
+                    data_by_chantier[i].append(json_data)
+                except json.JSONDecodeError:
+                    print("Couldn't decode file {}".format(json_file))
     return 0
 
 heatmap()
