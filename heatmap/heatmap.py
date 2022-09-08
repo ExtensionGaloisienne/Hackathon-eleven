526b50da221712fe26e3fa2c468980f34a6de1ad
Leonard Grellier <leonard.grellier@gmail.com>
2022-09-08 17:22:54 +0200

    heatmap commence a ressembler a qqch

--- heatmap/heatmap.py
+++ heatmap/heatmap.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 NAMES = ["BioSAV", "Devisubox", "Marseille", "Nouveau Campus", "Roissy"]
+SIZES = [[720, 1280], [720, 1280], [720, 1280], [1080, 1920], [1080, 1920]]
 
 
 # Choix d'un fichier aléatoire
@@ -42,12 +43,12 @@ def heatmap():
     roissy_files = filter_by_chantier(json_files, "Roissy")
     print("Total: {}, BioSAV: {}, Devisubox: {}, Marseille: {}\
 , Nouveau Campus: {}, Roissy: {}".format(len(json_files),
-                                             len(biosav_files),
-                                             len(devisubox_files),
-                                             len(marseille_files),
-                                             len(nouveaucampus_files),
-                                             len(roissy_files)))
-    chantiers = [devisubox_files, marseille_files, biosav_files, nouveaucampus_files, roissy_files]
+                                            len(biosav_files),
+                                            len(devisubox_files),
+                                            len(marseille_files),
+                                            len(nouveaucampus_files),
+                                            len(roissy_files)))
+    chantiers = [biosav_files,  devisubox_files, marseille_files, nouveaucampus_files, roissy_files]
     data_by_chantier = [[] for x in chantiers]
     for i in range(len(chantiers)):
         for json_file in chantiers[i]:
@@ -95,6 +96,9 @@ def extract_people(picture_data):
 def compute_center(a, b):
     return np.array([(a[0]+b[0]) // 2, (a[1]+b[1]) // 2])
 
+def compute_radius(a, b):
+    return np.mean([abs(b[0]-a[0]) / 2, abs(b[1]-a[1]) / 2])
+
 
 def get_boxes_from_people_list(people_list):
     return np.array([guy['points']['exterior'] for guy in people_list], dtype=np.int64)
@@ -113,13 +117,19 @@ def main():
     for i in range(5):
         plt.figure()
         plt.title(NAMES[i])
+        all_the_people = np.empty((0, 2, 2))
         for people_list in people_by_chantier[i]:
             people_boxes = get_boxes_from_people_list(people_list)
-            box_count = len(people_boxes)
-            centers = np.zeros((box_count, 2), dtype=np.int64)
-            for j in range(box_count):
-                centers[j] = compute_center(people_boxes[j][0], people_boxes[j][1])
-            plt.plot(centers[:, 0], centers[:, 1], 'bs')
+            all_the_people = np.concatenate((all_the_people, people_boxes))
+        box_count = len(all_the_people)
+        centers = np.zeros((box_count, 2), dtype=np.int64)
+        radii = np.zeros(box_count, dtype=np.int64)
+        for j in range(box_count):
+            centers[j] = compute_center(all_the_people[j][0], all_the_people[j][1])
+            radii[j] = compute_radius(all_the_people[j][0], all_the_people[j][1])
+        limits = [[0, SIZES[i][0]], [0, SIZES[i][1]]]
+        plt.hist2d(centers[:, 0], centers[:, 1], bins=50, weights=radii,
+                   range=limits, cmap='Blues')
         plt.show()
     return 0
 
